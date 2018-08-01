63acc6f93ea028575b2435b8eb54932c303d3036
bert <btemme@hotmail.com>
2018-08-01 14:17:45 +0200

    initial coding

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace tilegrab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // bounding box (amsterdam as sample) tip use bboxfinder.com
+            var xmin = 4.814930;
+            var ymin = 52.320757;
+            var xmax = 4.988308;
+            var ymax = 52.396763;
+
+            // remote server to download from
+            var url = $"https://geodata.nationaalgeoregister.nl/beta/topotiles/";
+            var extension = "pbf";
+
+            var tiles = TileHelper.GetTilesInArea(xmin, ymin, xmax, ymax);
+            var sep = Path.DirectorySeparatorChar;
+            var wc = new WebClient();
+            foreach (var t in tiles)
+            {
+                var di = Directory.CreateDirectory($"tiles{sep}{t.Z}{sep}{t.X}");
+                var filename = di.FullName + sep + t.Y + "." + extension;
+                wc.DownloadFile(url + $"{ t.Z}/{ t.X}/{t.Y}.{extension}", filename);
+                Console.WriteLine(url);
+
+            }
+            wc.Dispose();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TileHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Tiles.Tools;
+
+namespace tilegrab
+{
+    public class TileHelper
+    {
+        public static List<Tile> GetTilesInArea(double minx, double miny, double maxx, double maxy)
+        {
+            var res = new List<Tile>();
+            for (var z = 0; z <= 17; z++)
+            {
+                var tile = Tilebelt.PointToTile(minx, miny, z);
+                var tile1 = Tilebelt.PointToTile(maxx, maxy, z);
+
+                for (var x = tile.X; x <= tile1.X; x++)
+                {
+                    for (var y = tile1.Y; y <= tile.Y; y++)
+                    {
+                        var t = new Tile(x, y, z);
+                        res.Add(t);
+                    }
+
+                }
+            }
+            return res;
+        }
+    }
+}
